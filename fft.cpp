ed0612c4bedfd2832c877f6dc7a53bd70a5e0ea3
Tanvir Tareq <cse.tanvir.17@gmail.com>
2020-02-25 01:04:08 +0600

    alkj

--- fft.cpp
+++ fft.cpp
@@ -1,31 +1,24 @@
-
-
-
+#include<bits/stdc++.h>
+using namespace std;
 using cd = complex<double>;
 const double PI = acos(-1);
-
-
-/*
-when invert is true this function will act as antidft otherwise dft
+/**
+when invert is true this function will act as inverse dft otherwise dft
 when invert is false
-    it take an array of coeffecients and return as output array
-
+    it take an array of co-efficient  and return as output array
 when invert is true
-    it take an array of output and return array of coeffecient 
+    it take an array of output and return array of co-efficient
 */
 void fft(vector<cd> & a, bool invert) {
     int n = a.size();
-
     for (int i = 1, j = 0; i < n; i++) {
         int bit = n >> 1;
         for (; j & bit; bit >>= 1)
             j ^= bit;
         j ^= bit;
-
         if (i < j)
             swap(a[i], a[j]);
     }
-
     for (int len = 2; len <= n; len <<= 1) {
         double ang = 2 * PI / len * (invert ? -1 : 1);
         cd wlen(cos(ang), sin(ang));
@@ -45,16 +38,14 @@ void fft(vector<cd> & a, bool invert) {
             x /= n;
     }
 }
-
-
-/*
-this function take to array of coeffecient of two polynomial
-and return an array of coeffecient of a polynomial which is multiplication of previous polynomials
+/**
+this function take to array of coefficient of two polynomial
+and return an array of coefficient of a polynomial which is multiplication of previous polynomials
 */
 vector<int> multiply(vector<int> const& a, vector<int> const& b) {
     vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
     int n = 1;
-    while (n < a.size() + b.size()) 
+    while (n < a.size() + b.size())
         n <<= 1;
     fa.resize(n);
     fb.resize(n);
@@ -64,7 +55,6 @@ vector<int> multiply(vector<int> const& a, vector<int> const& b) {
     for (int i = 0; i < n; i++)
         fa[i] *= fb[i];
     fft(fa, true);
-
     vector<int> result(n);
     for (int i = 0; i < n; i++)
         result[i] = round(fa[i].real());
@@ -72,18 +62,15 @@ vector<int> multiply(vector<int> const& a, vector<int> const& b) {
 }
 
 
-
-// ntt
-/*
+/**
+ntt
 calculate those constant first
 calculation:
 mod=c*2^k+1
 find any c and k;
 find g = primitive root of mod
 find root=g^c
-find root_1=mudularr multiplicative inverse root 
-            =(1/root)%mod
-
+find root_1=modular multiplicative inverse root=(1/root)%mod
 find root_pw=2^k
 */
 const int mod = 7340033;
@@ -99,11 +86,9 @@ void fft(vector<int> & a, bool invert) {
         for (; j & bit; bit >>= 1)
             j ^= bit;
         j ^= bit;
-
         if (i < j)
             swap(a[i], a[j]);
     }
-
     for (int len = 2; len <= n; len <<= 1) {
         int wlen = invert ? root_1 : root;
         for (int i = len; i < root_pw; i <<= 1)
@@ -125,4 +110,35 @@ void fft(vector<int> & a, bool invert) {
         for (int & x : a)
             x = (int)(1LL * x * n_1 % mod);
     }
+}
+
+/**
+primitive root generator
+*/
+
+int generator (int p) {
+    vector<int> fact;
+    int phi = p-1,  n = phi;
+    for (int i=2; i*i<=n; ++i)
+        if (n % i == 0) {
+            fact.push_back (i);
+            while (n % i == 0)
+                n /= i;
+        }
+    if (n > 1)
+        fact.push_back (n);
+
+    for (int res=2; res<=p; ++res) {
+        bool ok = true;
+        for (size_t i=0; i<fact.size() && ok; ++i)
+            ok &= powmod (res, phi / fact[i], p) != 1;
+        if (ok)  return res;
+    }
+    return -1;
+}
+
+int main()
+{
+    cout<<generator(7340033)<<endl;
+    return 0;
 }
